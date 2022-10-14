2e0ce8dce9f56825b2386c4e0dc3200c7fab153f
Bert Gerding <bert.training@outlook.com>
2022-10-14 19:41:37 +0200

    Changed backend init config file

--- infra/terraform/aws-backend/main.tf
+++ infra/terraform/aws-backend/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "eu-central-1"
+  region = "eu-west-1"
 
   default_tags {
     tags = {
@@ -8,33 +8,33 @@ provider "aws" {
   }
 }
 
-resource "aws_s3_bucket" "zlrx-terraform-state" {
-  bucket        = "zlrx-terraform-state"
+resource "aws_s3_bucket" "gbg-terraform-state" {
+  bucket        = "gbg-terraform-state"
   force_destroy = true
 }
 
 resource "aws_s3_bucket_acl" "this" {
-  bucket = aws_s3_bucket.zlrx-terraform-state.id
+  bucket = aws_s3_bucket.gbg-terraform-state.id
   acl    = "private"
 }
 
 resource "aws_s3_bucket_versioning" "this" {
-  bucket = aws_s3_bucket.zlrx-terraform-state.id
+  bucket = aws_s3_bucket.gbg-terraform-state.id
   versioning_configuration {
     status = "Enabled"
   }
 }
 
 resource "aws_s3_bucket_public_access_block" "this" {
-  bucket                  = aws_s3_bucket.zlrx-terraform-state.id
+  bucket                  = aws_s3_bucket.gbg-terraform-state.id
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
 
-resource "aws_dynamodb_table" "zlrx-terraform-state-lock" {
-  name           = "zlrx-terraform-state-lock"
+resource "aws_dynamodb_table" "gbg-terraform-state-lock" {
+  name           = "gbg-terraform-state-lock"
   read_capacity  = 20
   write_capacity = 10
   hash_key       = "LockID"
